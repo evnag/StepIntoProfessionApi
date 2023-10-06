d74bf56482bfe8b9113951f404bd8e12489a6f8b
evnag <vskonev22@gmail.com>
2023-10-06 13:07:41 +0300

    Initial commit

---
+++ src/main/java/com/stepintoprofession/registration_service/StepIntoProfessionApiApplication.java
@@ -0,0 +1,13 @@
+package com.stepintoprofession.registration_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepIntoProfessionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StepIntoProfessionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stepintoprofession/registration_service/StepIntoProfessionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stepintoprofession.registration_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StepIntoProfessionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
